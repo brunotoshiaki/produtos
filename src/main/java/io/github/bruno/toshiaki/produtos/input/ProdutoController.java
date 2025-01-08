cb086a39c59f37119e7607366000055c468be2a1
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2025-01-08 20:53:29 -0300

    Adicionando restante do swagger

--- src/main/java/io/github/bruno/toshiaki/produtos/input/ProdutoController.java
+++ src/main/java/io/github/bruno/toshiaki/produtos/input/ProdutoController.java
@@ -4,6 +4,9 @@ import io.github.bruno.toshiaki.produtos.core.model.ProdutoDTO;
 import io.github.bruno.toshiaki.produtos.core.model.ProdutoResponse;
 import io.github.bruno.toshiaki.produtos.core.service.ProdutoService;
 import io.github.bruno.toshiaki.produtos.mapper.ProdutoResponseMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.Link;
@@ -29,12 +32,19 @@ public class ProdutoController {
     private final ProdutoResponseMapper produtoResponseMapper;
 
     @PostMapping
+    @Operation(summary = "Cadastrar", description = "Cadastra um Produto")
+    @ApiResponse(responseCode = "200", description = "Produto Cadastrado com Sucesso.")
     public ResponseEntity<Void> cadastrar(@RequestBody ProdutoDTO produto) {
         produtoService.salvar(produto);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Detalha", description = "Detalha  um Produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto Detalhado."),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado.")
+    })
     public ResponseEntity<ProdutoResponse> detalhe(@PathVariable("id") Long id) {
         var produto = produtoService.buscarPorId(id);
         var uri = ServletUriComponentsBuilder
@@ -42,21 +52,27 @@ public class ProdutoController {
                 .path("/{title}")
                 .path("/{image}")
                 .path("/{price}")
-
                 .buildAndExpand(produto.getTitle(), produto.getImage(), produto.getPrice()).toUri();
-     var response = produtoResponseMapper.fromEntity(produto);
+        var response = produtoResponseMapper.fromEntity(produto);
 
-      response.add(Link.of(uri.toString()));
+        response.add(Link.of(uri.toString()));
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/")
+    @Operation(summary = "Listar", description = "Lista todos os Produtos por página")
+    @ApiResponse(responseCode = "200")
     public ResponseEntity<List<ProdutoResponse>> listagem(@RequestParam(value = "page", defaultValue = "0") Integer page) {
         var result = produtoService.buscarPaginada(page);
         return ResponseEntity.ok().body(result.getContent());
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar", description = "Atualiza  um Produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto Detalhado."),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado.")
+    })
     ResponseEntity<Void> atualizar(@PathVariable("id") Long id, @RequestBody ProdutoDTO produto) {
         produtoService.atualizar(id, produto);
         return ResponseEntity.ok().build();
--- src/main/java/io/github/bruno/toshiaki/produtos/input/ProdutoFavoritoController.java
+++ src/main/java/io/github/bruno/toshiaki/produtos/input/ProdutoFavoritoController.java
@@ -3,6 +3,9 @@ package io.github.bruno.toshiaki.produtos.input;
 import io.github.bruno.toshiaki.produtos.core.model.ProdutoFavoritoDTO;
 import io.github.bruno.toshiaki.produtos.core.model.ProdutoFavoritoResponse;
 import io.github.bruno.toshiaki.produtos.core.service.ProdutoFavoritoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,17 +27,32 @@ public class ProdutoFavoritoController {
     private final ProdutoFavoritoService produtoFavoritoService;
 
     @PostMapping
+    @Operation(summary = "Favoritar", description = "Cadastra um Produto como favorito")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto adicionado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Cliente nao encontrado."),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado."),
+            @ApiResponse(responseCode = "422", description = "Produto ja favoritado."),
+    })
     public ResponseEntity<Void> cadastrar(@RequestBody ProdutoFavoritoDTO produto) {
         produtoFavoritoService.adicionarCarrinho(produto);
         return ResponseEntity.ok().build();
     }
+
+    @Operation(summary = "Remover", description = "Remove um Produto como favorito")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Produto removido com sucesso."),
+            @ApiResponse(responseCode = "422", description = "Não é possivel remover do favorito."),
+    })
     @DeleteMapping("/{id}/{idProduto}")
     public ResponseEntity<Void> remover(@PathVariable Long id, @PathVariable Long idProduto) {
         produtoFavoritoService.removeCarrinho(id,idProduto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Listar", description = "Listar todos os produtos de um cliente")
+    @ApiResponse(responseCode = "200")
     public ResponseEntity<ProdutoFavoritoResponse> listar(@RequestParam(value = "page", defaultValue = "0") Integer page,
                                                           @RequestParam(value = "size", defaultValue = "10") Integer size,
                                                           @PathVariable("id") Long id) {
